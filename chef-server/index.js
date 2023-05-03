091ab861945062981660b4ec8ab8b953cc2d5849
AhaD <abuahad35188@gmail.com>
2023-05-03 10:43:11 +0600

    food data added

--- chef-server/index.js
+++ chef-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
  const app =express();
  port = 5000;
  const data = require('./Data/data.json')
+ const food = require('./Data/food.json')
 
  const cors = require('cors')
  app.use(cors())
@@ -16,6 +17,14 @@ const express = require('express');
  })
 app.get('/data',(req,res)=>{
      res.send(data)
+})
+app.get('/food/:id',(req,res)=>{
+     const id = req.params.id
+     const foods = food.filter(f1=>f1.id == id)
+     res.send(foods)
+})
+app.get('/food',(req,res)=>{
+     res.send(food)
 })
 
 
